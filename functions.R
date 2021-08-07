11ae3c5dbc1dc6c4046b92af3521ce69cfed10cd
Ben Baldwin <1425357+guga31bb@users.noreply.github.com>
2021-08-07 14:05:37 -0400

    18 week season

--- functions.R
+++ functions.R
@@ -13,15 +13,19 @@ data_repo <- git2r::repository('./') # Set up connection to repository folder
 
 #get completed games
 get_finished_games <- function() {
-  
+
   names <- nflfastR::teams_colors_logos %>%
     dplyr::select(team_nick, team_abbr)
-  
+
   games <- readRDS(url("http://www.habitatring.com/games.rds")) %>%
     tibble::as_tibble() %>%
     dplyr::select(game_id, result, season, game_type, week, away_team, home_team) %>%
-    dplyr::mutate(week = as.integer(week), 
-           week = dplyr::if_else(game_type == 'REG', week, as.integer(week - 17)),
+    dplyr::mutate(week = as.integer(week),
+           week = dplyr::case_when(
+             game_type != 'REG' & season <= 2020 ~ as.integer(week - 17),
+             game_type != 'REG' & season > 2021 ~ as.integer(week - 18),
+             TRUE ~ week
+             ),
            game_type = dplyr::if_else(game_type == 'REG', 'reg', 'post')
     ) %>%
     dplyr::left_join(names, by = c('home_team' = 'team_abbr')) %>%
@@ -49,15 +53,15 @@ get_finished_games <- function() {
         url
       )
     )
-  
+
   return(games)
-  
+
 }
 
 #function to get the completed games
 #that are not present in the data repo
 get_missing_games <- function(finished_games, dir) {
-  
+
   server <- list.files(dir, recursive = T) %>%
     tibble::as_tibble() %>%
     dplyr::rename(
@@ -71,7 +75,7 @@ get_missing_games <- function(finished_games, dir) {
       season =
         stringr::str_extract(
           name, '[0-9]{4}'
-        ),
+        ) %>% as.integer(),
       week =
         as.integer(stringr::str_extract(name, '(?<=\\_)[0-9]{2}(?=\\_)'))
       ,
@@ -83,23 +87,24 @@ get_missing_games <- function(finished_games, dir) {
         stringr::str_extract(
           name, '(?<=[A-Z]\\_)[A-Z]{2,3}'
         ),
-      season_type = dplyr::if_else(week <= 17, 'REG', 'POST')
-    ) %>%
-    dplyr::mutate(
-      season = as.integer(season)
+      season_type = dplyr::case_when(
+        week > 17 & season <= 2020 ~ "POST",
+        week > 18 & season > 2020 ~ "POST",
+        TRUE ~ "REG"
+        )
     ) %>%
     dplyr::arrange(season, week) %>%
     dplyr::rename(game_id = name) %>%
     dplyr::distinct() %>%
     dplyr::arrange(game_id)
-  
+
   server_ids <- unique(server$game_id)
   finished_ids <-unique(finished_games$game_id)
-  
+
   need_scrape <- finished_games[!finished_ids %in% server_ids,]
-  
+
   message(glue::glue('{lubridate::now()}: You have {nrow(finished_games[finished_ids %in% server_ids,])} games and need {nrow(need_scrape)}'))
-  
+
   return(need_scrape)
-  
+
 }
